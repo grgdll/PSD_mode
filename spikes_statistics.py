c66e1b5a1a51964478e3241b46c8df4f7e7e3c74
grg.dll <grg.dll@gmail.com>
2024-11-27 14:18:29 +0100

    first commit

---
+++ spikes_statistics.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import glob
+import numpy as np
+import optics_rig as orig
+from scipy.interpolate import interp1d
+from scipy import signal as signal
+from scipy.stats import gamma, burr12
+
+from iteration_utilities import flatten
+
+import os
+import glob
+from scipy import optimize
+import copy
+import dill
+
+import colorcet as cc
+
+from scipy.integrate import trapz
+
+
+
